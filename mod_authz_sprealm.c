4032260695a900bf144e9729e4c79b2350a482eb
Alessandro Lorenzi <alessandro.lorenzi@gmail.com>
2012-12-20 10:10:00 +0100

    Group -> realm

--- mod_authz_sprealm.c
+++ mod_authz_sprealm.c
@@ -1,4 +1,11 @@
-/* Copyright 2008 Jan Wolter - See LICENSE and NOTICE */
+/*
+ * Modifica di mod_authz_unixgroup.c per controllare il permesso
+ * con i gruppi di securepass
+ * Author: Alessandro Lorenzi <alessandro.lorenzi@garl.ch>
+ *
+ * 
+ *
+ * */
 
 #include "apr_lib.h"
 
@@ -30,7 +37,7 @@
  * Structure for the module itself.  The actual definition of this structure
  * is at the end of the file.
  */
-module AP_MODULE_DECLARE_DATA authz_unixgroup_module;
+module AP_MODULE_DECLARE_DATA authz_securepassgroup_module;
 
 /* A handle for retrieving the requested file's group from mod_authnz_owner */
 APR_DECLARE_OPTIONAL_FN(char*, authz_owner_get_file_group, (request_rec *r));
@@ -42,71 +49,31 @@ APR_DECLARE_OPTIONAL_FN(char*, authz_owner_get_file_group, (request_rec *r));
  * be a unix login corresponding to the named user.
  */
 
-static int check_unix_group(request_rec *r, const char *grouplist)
+static int check_securepass_group(request_rec *r, const char *grouplist)
 {
     // Al posto di questo fare il match del nome.
     char **p;
     struct group *grp;
     char *user= r->user;
-    char *w, *at;
+    char *realm,*w, *at;
 
-    /* Strip @ sign and anything following it from the username.  Some
-     * authentication modules, like mod_auth_kerb like appending such
-     * stuff to user names, but an @ sign is never legal in a unix login
-     * name, so it should be safe to always discard such stuff.
-     */
-    if ((at= strchr(user, '@')) != NULL) *at= '\0';
-
-    /* Get info about login */
-    struct passwd *pwd= getpwnam(user);
-    if (pwd == NULL)
-    {
-	/* No such user - forget it */
-	if (at != NULL) *at= '@';
-    	return 0;
-    }
-
+	// estrapolo il realm dell'utente
+	realm=strchr(str,'@');
+	realm++;
+	
     /* Loop through list of groups passed in */
     while (*grouplist != '\0')
     {
-	w= ap_getword_conf(r->pool, &grouplist);
-	if (apr_isdigit(w[0]))
-	{
-	    /* Numeric group id */
-	    int gid= atoi(w);
+		// controlla la lista dei gruppi nella configurazione.
+		w= ap_getword_conf(r->pool, &grouplist);
+		// in w dovrebbe esserci il gruppo autorizzato
+		// devo vedere se realm e w sono uguali
 
-	    /* Check if it matches the user's primary group */
-	    if (gid == pwd->pw_gid)
-	    {
-		if (at != NULL) *at= '@';
-		return 1;
-	    }
-
-	    /* Get list of group members for numeric group id */
-	    grp= getgrgid(gid);
-	}
-	else
-	{
-	    /* Get gid and list of group members for group name */
-	    grp= getgrnam(w);
-	    /* Check if gid of this group matches user's primary gid */
-	    if (grp != NULL && grp->gr_gid == pwd->pw_gid)
-	    {
-		if (at != NULL) *at= '@';
-		return 1;
-	    }
-	}
-
-	/* Walk through list of members, seeing if any match user login */
-	if (grp != NULL)
-	    for (p= grp->gr_mem; *p != NULL; p++)
-	    {
-		if (!strcmp(user, *p))
+		/* Walk through list of members, seeing if any match user login */
+		if (realm.compare(w)==0)
 		{
-		    if (at != NULL) *at= '@';
-		    return 1;
+			return 1;
 		}
-	    }
     }
 
     /* Didn't find any matches, flunk him */
@@ -114,26 +81,31 @@ static int check_unix_group(request_rec *r, const char *grouplist)
     return 0;
 }
 
-static authz_status unixgroup_check_authorization(request_rec *r,
+
+
+static authz_status securepassgroup_check_authorization(request_rec *r,
         const char *require_args, const void *parsed_require_args)
 {
     /* If no authenticated user, pass */
     if ( !r->user ) return AUTHZ_DENIED_NO_USER;
 
-    if (check_unix_group(r,require_args))
+    if (check_securepass_group(r,require_args))
 	return AUTHZ_GRANTED;
 
     ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
         "Authorization of user %s to access %s failed. "
-        "User not in Required unix groups (%s).",
+        "User not in Required securepass groups (%s).",
         r->user, r->uri, require_args);
 
     return AUTHZ_DENIED;
 }
 
-APR_OPTIONAL_FN_TYPE(authz_owner_get_file_group) *authz_owner_get_file_group;
 
-static authz_status unixfilegroup_check_authorization(request_rec *r,
+/*   da qui in poi dovrebbe riguardare i file, quindi... ¿¿??  */
+
+APR_OPTIONAL_FN_TYPE(authz_ownsecurepasser_get_file_group) *authz_owner_get_file_group;
+
+static authz_status securepassfilegroup_check_authorization(request_rec *r,
         const char *require_args, const void *parsed_require_args)
 {
     const char *filegroup= NULL;
@@ -148,50 +120,50 @@ static authz_status unixfilegroup_check_authorization(request_rec *r,
         /* No errog log entry, because mod_authz_owner already made one */
         return AUTHZ_DENIED;
 
-    if (check_unix_group(r,filegroup))
+    if (check_securepass_group(r,filegroup))
 	return AUTHZ_GRANTED;
     
     ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
         "Authorization of user %s to access %s failed. "
-        "User not in Required unix file group (%s).",
+        "User not in Required securepass file group (%s).",
         r->user, r->uri, filegroup);
 
     return AUTHZ_DENIED;
 }
 
-static const authz_provider authz_unixgroup_provider =
+static const authz_provider authz_securepassgroup_provider =
 {
-    &unixgroup_check_authorization,
+    &securepassgroup_check_authorization,
     NULL,
 };
 
-static const authz_provider authz_unixfilegroup_provider =
+static const authz_provider authz_securepassfilegroup_provider =
 {
-    &unixfilegroup_check_authorization,
+    &securepassfilegroup_check_authorization,
     NULL,
 };
 
-static void authz_unixgroup_register_hooks(apr_pool_t *p)
+static void authz_securepassgroup_register_hooks(apr_pool_t *p)
 {
     /* Get a handle on mod_authz_owner */
     authz_owner_get_file_group = APR_RETRIEVE_OPTIONAL_FN(authz_owner_get_file_group);
 
     /* Register authz providers */
-    ap_register_auth_provider(p, AUTHZ_PROVIDER_GROUP, "unix-group",
+    ap_register_auth_provider(p, AUTHZ_PROVIDER_GROUP, "securepass-group",
             AUTHZ_PROVIDER_VERSION,
-            &authz_unixgroup_provider, AP_AUTH_INTERNAL_PER_CONF);
+            &authz_securepassgroup_provider, AP_AUTH_INTERNAL_PER_CONF);
 
-    ap_register_auth_provider(p, AUTHZ_PROVIDER_GROUP, "unix-file-group",
+    ap_register_auth_provider(p, AUTHZ_PROVIDER_GROUP, "securepass-file-group",
             AUTHZ_PROVIDER_VERSION,
-            &authz_unixfilegroup_provider, AP_AUTH_INTERNAL_PER_CONF);
+            &authz_securepassfilegroup_provider, AP_AUTH_INTERNAL_PER_CONF);
 }
     
-module AP_MODULE_DECLARE_DATA authz_unixgroup_module = {
+module AP_MODULE_DECLARE_DATA authz_securepassgroup_module = {
     STANDARD20_MODULE_STUFF,
     NULL,				  /* create per-dir config */
     NULL,			          /* merge per-dir config */
     NULL,			          /* create per-server config */
     NULL,			          /* merge per-server config */
     NULL,		         	  /* command apr_table_t */
-    authz_unixgroup_register_hooks        /* register hooks */
+    authz_securepassgroup_register_hooks        /* register hooks */
 };
